4d92141e997d9357fdf2a9afb4cd4b2376627556
Gennadiy Kozlenko <gennadiy.kozlenko@gmail.com>
2016-09-24 10:47:47 +0300

    Parse new atoms, add resources for testing

--- test/tests.js
+++ test/tests.js
@@ -3,47 +3,16 @@
 var MP4Parser = require('../index');
 
 var fs = require('fs');
-var http = require('http');
-var shortid = require('shortid');
-var Promise = require('bluebird');
-
 var chai = require('chai');
 chai.use(require('chai-as-promised'));
 var expect = chai.expect;
 
-var TEST_FILE = 'http://s3-eu-west-1.amazonaws.com/hwdmedia-resources/boomstream.mp4';
+var TEST_FILE = './resources/boomstream.mp4';
 
-var tmpFile = null;
 var movie = null;
-
-tmpFile = '/tmp/ByuvCbXa.mp4';
 
 describe('node-mp4-parser', function () {
     this.timeout(10000);
-
-    // before(function() {
-    //     // Download file
-    //     return new Promise(function(resolve) {
-    //         tmpFile = '/tmp/' + shortid.generate() + '.mp4';
-    //         var file = fs.createWriteStream(tmpFile);
-    //         http.get(TEST_FILE, function(response) {
-    //             response.pipe(file).on('close', function() {
-    //                 resolve();
-    //             });
-    //         });
-    //     });
-    // });
-    //
-    // after(function() {
-    //     // Delete file
-    //     if (tmpFile !== null) {
-    //         return new Promise(function(resolve) {
-    //             fs.unlink(tmpFile, function() {
-    //                 resolve();
-    //             });
-    //         });
-    //     }
-    // });
 
     describe('parse', function () {
 
@@ -52,7 +21,7 @@ describe('node-mp4-parser', function () {
         });
 
         it('should not throw an error when opens an existing file', function () {
-            return expect(MP4Parser.parse(tmpFile)).to.be.fulfilled;
+            return expect(MP4Parser.parse(TEST_FILE)).to.be.fulfilled;
         });
 
     });
@@ -60,7 +29,7 @@ describe('node-mp4-parser', function () {
     describe('length', function () {
 
         beforeEach(function() {
-            return MP4Parser.parse(tmpFile).then(function(data) {
+            return MP4Parser.parse(TEST_FILE).then(function(data) {
                 movie = data;
             });
         });
@@ -70,7 +39,7 @@ describe('node-mp4-parser', function () {
         });
 
         it('should return the right file length', function () {
-            return expect(movie.size).to.equal(fs.statSync(tmpFile)['size']);
+            return expect(movie.size).to.equal(fs.statSync(TEST_FILE)['size']);
         });
 
     });
