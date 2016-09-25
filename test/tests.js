bc8e503352132eaa41fe903077b93f3234e79275
pipll <gennadiy.kozlenko@gmail.com>
2016-09-25 03:01:26 +0300

    Parse some atoms

--- test/tests.js
+++ test/tests.js
@@ -2,17 +2,27 @@
 
 var MP4Parser = require('../index');
 
-var fs = require('fs');
 var chai = require('chai');
 chai.use(require('chai-as-promised'));
 var expect = chai.expect;
 
-var TEST_FILE = './resources/boomstream.mp4';
+var MP4_FILE = './resources/boomstream.mp4';
+var INVALID_FILE = './resources/picture.jpg';
 
 var movie = null;
 
 describe('node-mp4-parser', function () {
     this.timeout(10000);
+
+    beforeEach(function() {
+        return MP4Parser.parse(MP4_FILE).then(function(data) {
+            movie = data;
+        });
+    });
+
+    afterEach(function() {
+        return movie.close();
+    });
 
     describe('parse', function () {
 
@@ -20,26 +30,36 @@ describe('node-mp4-parser', function () {
             return expect(MP4Parser.parse('/file/not/exists.mp4')).to.be.rejected;
         });
 
+        it('should throw an error when opens a not mp4 file', function () {
+            return expect(MP4Parser.parse(INVALID_FILE)).to.be.rejected;
+        });
+
         it('should not throw an error when opens an existing file', function () {
-            return expect(MP4Parser.parse(TEST_FILE)).to.be.fulfilled;
+            return expect(MP4Parser.parse(MP4_FILE)).to.be.fulfilled;
         });
 
     });
 
-    describe('length', function () {
+    describe('size', function () {
 
-        beforeEach(function() {
-            return MP4Parser.parse(TEST_FILE).then(function(data) {
-                movie = data;
-            });
+        it('should return right file length', function () {
+            return expect(movie.size).to.equal(16068581);
         });
 
-        afterEach(function() {
-            return movie.close();
+    });
+
+    describe('timescale', function () {
+
+        it('should return right video timescale', function () {
+            return expect(movie.timescale).to.equal(1000);
         });
 
-        it('should return the right file length', function () {
-            return expect(movie.size).to.equal(fs.statSync(TEST_FILE)['size']);
+    });
+
+    describe('duration', function () {
+
+        it('should return right video duration', function () {
+            return expect(movie.duration).to.equal(61320);
         });
 
     });
