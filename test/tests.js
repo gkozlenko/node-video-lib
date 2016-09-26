c89a2afc6a05c3359b79cb313948f1b079e675f7
pipll <gennadiy.kozlenko@gmail.com>
2016-09-26 23:51:59 +0300

    Refactoring, parse tracks

--- test/tests.js
+++ test/tests.js
@@ -9,59 +9,87 @@ var expect = chai.expect;
 var MP4_FILE = './resources/boomstream.mp4';
 var INVALID_FILE = './resources/picture.jpg';
 
-var movie = null;
-
 describe('node-mp4-parser', function () {
     this.timeout(10000);
 
-    beforeEach(function() {
-        return MP4Parser.parse(MP4_FILE).then(function(data) {
-            movie = data;
-        });
-    });
-
-    afterEach(function() {
-        return movie.close();
-    });
-
     describe('parse', function () {
-
         it('should throw an error when opens a non existing file', function () {
             return expect(MP4Parser.parse('/file/not/exists.mp4')).to.be.rejected;
         });
-
         it('should throw an error when opens a not mp4 file', function () {
             return expect(MP4Parser.parse(INVALID_FILE)).to.be.rejected;
         });
-
         it('should not throw an error when opens an existing file', function () {
             return expect(MP4Parser.parse(MP4_FILE)).to.be.fulfilled;
         });
-
     });
 
-    describe('size', function () {
+    describe('movie', function () {
+        var movie = null;
 
-        it('should return right file length', function () {
-            return expect(movie.size).to.equal(16068581);
+        before(function() {
+            return MP4Parser.parse(MP4_FILE).then(function(data) {
+                movie = data;
+            });
         });
 
-    });
-
-    describe('timescale', function () {
-
-        it('should return right video timescale', function () {
-            return expect(movie.timescale).to.equal(1000);
+        it('should return right timescale and duration', function () {
+            return expect(movie.duration / movie.timescale).to.be.within(61, 62);
+        });
+        it('should have two tracks', function () {
+            return expect(movie.tracks.length).to.equal(2);
         });
 
-    });
+        describe('videoTrack', function () {
+            var videoTrack = null;
 
-    describe('duration', function () {
+            before(function() {
+                videoTrack = movie.videoTrack();
+            });
 
-        it('should return right video duration', function () {
-            return expect(movie.duration).to.equal(61320);
+            it('should be present', function () {
+                return expect(videoTrack).to.be.ok;
+            });
+            it('should have right width', function () {
+                return expect(videoTrack.width).to.equal(1280);
+            });
+            it('should have right height', function () {
+                return expect(videoTrack.height).to.equal(720);
+            });
+            it('should have right timescale and duration', function () {
+                return expect(videoTrack.duration / videoTrack.timescale).to.be.within(61, 62);
+            });
+            it('should have extraData', function () {
+                return expect(videoTrack.extraData).to.be.ok;
+            });
         });
 
+        describe('audioTrack', function () {
+            var audioTrack = null;
+
+            before(function() {
+                audioTrack = movie.audioTrack();
+            });
+
+            it('should be present', function () {
+                return expect(audioTrack).to.be.ok;
+            });
+            it('should have right channels', function () {
+                return expect(audioTrack.channels).to.equal(2);
+            });
+            it('should have right sampleRate', function () {
+                return expect(audioTrack.sampleRate).to.equal(44100);
+            });
+            it('should have right sampleSize', function () {
+                return expect(audioTrack.sampleSize).to.equal(16);
+            });
+            it('should have right timescale and duration', function () {
+                return expect(audioTrack.duration / audioTrack.timescale).to.be.within(61, 62);
+            });
+            it('should have extraData', function () {
+                return expect(audioTrack.extraData).to.be.ok;
+            });
+        });
     });
 
 });
