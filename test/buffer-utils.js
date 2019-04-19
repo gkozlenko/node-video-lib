a7fbd953b7e84cd650a35f568fc30b31ddf313e4
Gennady Kozlenko <gennadiy.kozlenko@gmail.com>
2019-04-19 20:07:25 +0300

    Modify BufferUtils

--- test/buffer-utils.js
+++ test/buffer-utils.js
@@ -12,15 +12,24 @@ describe('BufferUtils', function () {
         it('should write and read ordinary integers', function () {
             let buffer = Buffer.allocUnsafe(8);
             let number = faker.random.number();
-            BufferUtils.writeInt64BE(buffer, number, 0);
-            expect(BufferUtils.readInt64BE(buffer, 0)).to.be.equal(number);
+            BufferUtils.writeUInt64BE(buffer, number, 0);
+            expect(BufferUtils.readUInt64BE(buffer, 0)).to.be.equal(number);
         });
 
         it('should write and read long integers', function () {
             let buffer = Buffer.allocUnsafe(8);
             let number = faker.random.number({min: 111111111111, max: 999999999999999});
-            BufferUtils.writeInt64BE(buffer, number, 0);
-            expect(BufferUtils.readInt64BE(buffer, 0)).to.be.equal(number);
+            BufferUtils.writeUInt64BE(buffer, number, 0);
+            expect(BufferUtils.readUInt64BE(buffer, 0)).to.be.equal(number);
+        });
+
+        it('should write and read boundary values', function () {
+            let buffer = Buffer.allocUnsafe(8);
+            let numbers = [0xFFFFFFFF - 2, 0xFFFFFFFF - 1, 0xFFFFFFFF, 0xFFFFFFFF + 1, 0xFFFFFFFF + 2];
+            for (let number of numbers) {
+                BufferUtils.writeUInt64BE(buffer, number, 0);
+                expect(BufferUtils.readUInt64BE(buffer, 0)).to.be.equal(number);
+            }
         });
     });
 
