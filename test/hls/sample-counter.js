388c2c1611d0d9eb4bbeee668e26eaf72c623e4b
Gennady Kozlenko <gennadiy.kozlenko@gmail.com>
2026-01-12 21:53:52 +0100

    HLS Packetizer optimization
    - Get rid of unessary copy data
    - Update sample counter to work with different pids

--- test/hls/sample-counter.js
+++ test/hls/sample-counter.js
@@ -1,27 +1,26 @@
 'use strict';
 
 const SampleCounter = require('../../lib/hls/sample-counter');
-const AudioSample = require('../../lib/audio-sample');
-const VideoSample = require('../../lib/video-sample');
 
 const chai = require('chai');
 const expect = chai.expect;
 
+const VIDEO_PID = 0x100;
+const AUDIO_PID = 0x101;
+
 describe('SampleCounter', function () {
     before(function () {
         this.counter = new SampleCounter();
-        this.audioSample = new AudioSample();
-        this.videoSample = new VideoSample();
     });
 
     describe('#next()', function () {
         it('should return a right sequence for different sample types', function () {
             for (let i = 0; i < 5; i++) {
-                expect(this.counter.next(this.audioSample)).to.be.equal(i);
+                expect(this.counter.next(AUDIO_PID)).to.be.equal(i);
             }
             for (let i = 0; i < 512; i++) {
-                expect(this.counter.next(this.audioSample)).to.be.equal((i + 5) % 16);
-                expect(this.counter.next(this.videoSample)).to.be.equal(i % 16);
+                expect(this.counter.next(AUDIO_PID)).to.be.equal((i + 5) % 16);
+                expect(this.counter.next(VIDEO_PID)).to.be.equal(i % 16);
             }
         });
     });
